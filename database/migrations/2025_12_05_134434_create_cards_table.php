d8d99fbae274a91e4f8511b4b738c6285f34f350
Jan Philip Faith <fjanphilip9.jp@gmail.com>
2025-12-05 21:42:29 +0700

    feat: first commit

---
+++ database/migrations/2025_12_05_134434_create_cards_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('cards', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('set_id')->constrained('study_sets')->onDelete('cascade');
+            $table->string('japanese_word', 255);
+            $table->string('japanese_reading', 255);
+            $table->text('meaning');
+            $table->text('example_sentence')->nullable();
+            $table->string('pitch_accent', 255)->nullable();
+            $table->boolean('is_mastered')->default(false);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('cards');
+    }
+};
---
+++ database/migrations/2025_12_05_134732_create_user_card_progress_table.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('user_card_progress', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('user_id')->constrained()->onDelete('cascade');
+            $table->foreignId('card_id')->constrained()->onDelete('cascade');
+            $table->integer('review_count')->default(0);
+            $table->integer('correct_streak')->default(0);
+            $table->timestamp('next_review_date')->nullable();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('user_card_progress');
+    }
+};
